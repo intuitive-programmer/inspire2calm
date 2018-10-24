4229704fd4420ac9f1491fe04a823627e8b61960
intuitive-programmer <steven@intuitiveprogrammer.com>
2018-10-24 20:51:25 +0100

    Created tables with seed

--- db/migrate/20181024192435_create_users_categories_usercategory_tables.rb
+++ db/migrate/20181024192435_create_users_categories_usercategory_tables.rb
@@ -9,7 +9,7 @@ class CreateUsersCategoriesUsercategoryTables < ActiveRecord::Migration[5.2]
       t.integer :count
     end
 
-    create_table :user_category do |t|
+    create_table :user_categories do |t|
       t.integer :user_id
       t.integer :category_id
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2018_10_24_192435) do
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.integer "count"
+  end
+
+  create_table "user_categories", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "category_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+  end
 
 end
